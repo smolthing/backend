afa2a56ec5e7d7e48c26ac2112e87f5dcb6a0e48
mysmolthing <mysmolthing@gmail.com>
2023-08-08 02:23:04 +0800

    Add GetUser grpc endpoint

---
+++ src/main/java/com/backend/smolthing/grpc/GetUserHandler.java
@@ -0,0 +1,37 @@
+package com.backend.smolthing.grpc;
+
+import com.protobuf.schema.grpc.GetUserResponse;
+import com.protobuf.schema.grpc.User;
+import com.protobuf.schema.grpc.UserServiceGrpc;
+import io.vertx.grpc.common.GrpcStatus;
+import io.vertx.grpc.server.GrpcServer;
+import java.util.HashMap;
+import java.util.Map;
+
+public class GetUserHandler {
+  private static final Map<Long, String> listOfUsers = new HashMap<>();
+
+  static {
+    listOfUsers.put(1L, "smol");
+    listOfUsers.put(2L, "small");
+    listOfUsers.put(3L, "tiny");
+  }
+  public static void handle(GrpcServer grpcServer) {
+    grpcServer.callHandler(UserServiceGrpc.getGetUserMethod(), request -> {
+
+      request.handler(req -> {
+        final long userId = req.getId();
+        final boolean hasUsername = listOfUsers.containsKey(userId);
+
+        if (hasUsername) {
+          User user = User.newBuilder().setId(userId).setName(listOfUsers.get(userId)).build();
+          request.response().end(GetUserResponse.newBuilder().setUser(user).build());
+        } else {
+          request.response()
+            .status(GrpcStatus.NOT_FOUND)
+            .end();
+        }
+      });
+    });
+  }
+}
--- src/main/java/com/backend/smolthing/grpc/GrpcApiVerticle.java
+++ src/main/java/com/backend/smolthing/grpc/GrpcApiVerticle.java
@@ -1,8 +1,5 @@
 package com.backend.smolthing.grpc;
 
-import com.protobuf.schema.grpc.GetUserResponse;
-import com.protobuf.schema.grpc.User;
-import com.protobuf.schema.grpc.UserServiceGrpc;
 import io.vertx.core.AbstractVerticle;
 import io.vertx.grpc.server.GrpcServer;
 import java.io.IOException;
@@ -10,13 +7,11 @@ import java.io.IOException;
 public class GrpcApiVerticle extends AbstractVerticle {
   @Override
   public void start() throws IOException {
-    GrpcServer rpcServer = GrpcServer.server(vertx);
-    rpcServer.callHandler(UserServiceGrpc.getGetUserMethod(), request -> {
-      User user = User.newBuilder().setId(1).setName("smol").build();
-      request.response().end(GetUserResponse.newBuilder().setUser(user).build());
-    });
+    GrpcServer grpcServer = GrpcServer.server(vertx);
 
-    vertx.createHttpServer().requestHandler(rpcServer).listen(9000, grpc -> {
+   GetUserHandler.handle(grpcServer);
+
+    vertx.createHttpServer().requestHandler(grpcServer).listen(9000, grpc -> {
       if (grpc.succeeded()) {
         System.out.println("gRPC server is running on port 9000");
       } else {
