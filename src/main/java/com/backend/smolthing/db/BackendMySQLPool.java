f9e87adfd9582e88580246d0c7704e83f75d08de
mysmolthing <mysmolthing@gmail.com>
2023-08-13 02:40:43 +0800

    Add mysqlpool

---
+++ src/main/java/com/backend/smolthing/db/BackendMySQLPool.java
@@ -0,0 +1,29 @@
+package com.backend.smolthing.db;
+
+import io.vertx.core.Vertx;
+import io.vertx.mysqlclient.MySQLConnectOptions;
+import io.vertx.sqlclient.Pool;
+import io.vertx.sqlclient.PoolOptions;
+import javax.inject.Singleton;
+import lombok.Getter;
+
+@Singleton
+public class BackendMySQLPool {
+  @Getter
+  private final Pool pool;
+
+  public BackendMySQLPool(Vertx vertx) {
+    MySQLConnectOptions connectOptions = new MySQLConnectOptions()
+      .setPort(3306)
+      .setHost("localhost")
+      .setDatabase("backend")
+      .setUser("root")
+      .setPassword("password")
+      .setIdleTimeout(5);
+
+    PoolOptions poolOptions = new PoolOptions()
+      .setMaxSize(5);
+
+    pool = Pool.pool(vertx, connectOptions, poolOptions);
+  }
+}
